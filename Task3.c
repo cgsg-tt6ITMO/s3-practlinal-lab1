941b705c3867d616b54bca41cee118094f400755
ttoomat <troitskayatamara@mail.ru>
2023-09-27 15:30:16 +0300

    Word into array of size_t.

--- Task3.c
+++ Task3.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 
 #define abc_size 32
+#define code_len 5
 
 struct map* abc;
 
 void print_str(size_t* s) {
-  for (size_t i = 0; i < 5; i++) {
+  for (size_t i = 0; i < 20; i++) {
     printf("%zu", *(s + i));
   }
   printf(" ");
@@ -23,9 +24,8 @@ struct map {
 };
 
 struct map map(char* c, char letter) {
-  size_t n = 5;
-  size_t* code = (size_t*)malloc(sizeof(size_t) * n);
-  for (size_t i = 0; i < n; i++) {
+  size_t* code = (size_t*)malloc(sizeof(size_t) * code_len);
+  for (size_t i = 0; i < code_len; i++) {
     if (c[i] == '0') {
       code[i] = 0;
     }
@@ -72,13 +72,29 @@ void abc_init(void) {
   abc[i++] = map("11111", 'я');
 }
 
+size_t* get_letter_code(char letter) {
+  for (size_t i = 0; i < abc_size; i++)  {
+    if (abc[i].letter == letter) {
+      return abc[i].code;
+    }
+  }
+  printf("THERE'S NO SUCH LETTER\n");
+  return NULL;
+}
+
 int main() {
   system("chcp 1251");
   abc_init();
-  for (size_t i = 0; i < abc_size; i++) {
-    print_str(abc[i].code);
-    printf("%c\n", abc[i].letter);
-  }
+  size_t message_len = 4;
+  char* word = "тома";
+  size_t* nums = (size_t*)malloc(sizeof(size_t) * message_len * code_len);
 
+  for (size_t i = 0; i < message_len; i++) {
+    size_t* tmp = get_letter_code(word[i]);
+    for (size_t j = 0; j < code_len; j++) {
+      *(nums + code_len * i + j) = tmp[j];
+    }
+  }
+  print_str(nums);
   return 0;
 }
